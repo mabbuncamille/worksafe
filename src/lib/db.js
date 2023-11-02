9faad50a090d1c3322482eb7c2ef420809c02a8e
mabbuncamille <mabbuncamille@gmail.com>
2023-11-03 03:56:27 +0800

    Initial commit

---
+++ src/lib/db.js
@@ -0,0 +1,25 @@
+import mysql from 'serverless-mysql';
+
+const db = mysql({
+  library: require('mysql2'),
+  config: {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+  },
+});
+
+// ===TEMPORARY===
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async function executeQuery({ query, values }) {
+  try {
+    const results = await db.query(query, values);
+    await db.end();
+    return results;
+  } catch (error) {
+    throw error;
+    // results.status(500).json({ error: error.message });
+  }
+}
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import executeQuery from '../../lib/db';
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const result = await executeQuery({
+      query: 'SELECT * FROM `roles`',
+      values: [req.body.content],
+    });
+    console.log(result, 'TESTING: RESULT');
+    res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+  }
 }
