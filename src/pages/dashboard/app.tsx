325b75245706fa57f7635ee0cdc0d1033d86d26a
mabbuncamille <mabbuncamille@gmail.com>
2024-02-07 16:51:39 +0800

    Add dashboard page

--- src/pages/dashboard/app.tsx
+++ src/pages/dashboard/app.tsx
@@ -1,3 +1,10 @@
-export default function app() {
+import axios from '@/utils/axios';
+import { useEffect } from 'react';
+
+export default function App() {
+  useEffect(() => {
+    // const response = axios.get('/')
+  }, []);
+
   return <h1>DASHBOARD</h1>;
 }
