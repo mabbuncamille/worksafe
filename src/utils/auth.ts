822b31b58761318377d255644b79af0630289ce3
mabbuncamille <mabbuncamille@gmail.com>
2024-02-08 15:39:46 +0800

    Add decode token

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,7 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
 export function generateToken(payload: any) {
   return jwt.sign(payload, process.env.JWT_SECRET as string, {
-    expiresIn: '1m',
+    expiresIn: '24h',
   });
+}
+
+export function decodeToken(token: string, ignoreExpiration: boolean = false) {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET as string, {
+      ignoreExpiration,
+    }) as JwtPayload;
+  } catch (error) {
+    return null;
+  }
 }
