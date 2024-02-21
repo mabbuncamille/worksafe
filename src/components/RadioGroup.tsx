fed32a549b408315360ffdaff24d7fc8ed4c0a3b
mabbuncamille <mabbuncamille@gmail.com>
2024-02-21 09:19:28 +0800

    Create initial radio button group

---
+++ src/components/RadioGroup.tsx
@@ -0,0 +1,38 @@
+import { UseFormRegister } from 'react-hook-form';
+
+type RadioProps = {
+  name: string;
+  id: string;
+  label: string;
+  floors?: string[];
+};
+
+type RadioGroupProps = {
+  value: RadioProps[];
+  register: UseFormRegister<any>;
+};
+
+export default function RadioGroup({ value, register }: RadioGroupProps) {
+  return (
+    <div className="flex space-x-2">
+      {value.map((list, index) => (
+        <div key={index}>
+          <input
+            type="radio"
+            id={list.id}
+            value={list.id}
+            className="hidden peer"
+            {...register(list.name)}
+          />
+
+          <label
+            htmlFor={list.id}
+            className="border text-black bg-white text-md relative cursor-pointer inline-flex border-gray-200 rounded-md hover:bg-gray-100 peer-checked:text-white peer-checked:bg-primary w-20 py-2 justify-center"
+          >
+            {list.label}
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
